52707ad2a7d368bda8034fd099ba36935ce4479f
icestellar <1001angel2nd.cs@gmail.com>
2020-12-14 16:47:55 -0400

    manipulando strings

--- ByteModelos/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ ByteModelos/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -8,10 +8,29 @@ namespace ByteBank.SistemaAgencia
 {
     class ExtratorValorDeArgumentosURL
     {
+        public string URL { get; }
+        private readonly string _argumentos; 
+        //public ExtratorValorDeArgumentosURL(string url)
+        //{
+        //    if (url == null) throw new ArgumentNullException(nameof(url), "string nula");
+        //    if (url == "") throw new ArgumentException("Nao e permitido string vazia", nameof(url));
+        //}
         public ExtratorValorDeArgumentosURL(string url)
         {
-            if (url == null) throw new ArgumentNullException(nameof(url), "string nula");
-            if (url == "") throw new ArgumentException("Nao e permitido string vazia", nameof(url));
+            if (String.IsNullOrEmpty(url)) throw new ArgumentException("O argumento Url nao pode ser vazio ou nulo", nameof(url));
+            URL = url;
+            _argumentos = url.Substring(url.IndexOf("?")+1);
         }
+
+        public string GetValor(string nomeDoParametro)
+        {
+            string termo = nomeDoParametro.ToLower() + "=";
+            int indiceTermo = _argumentos.IndexOf(termo);
+            //string resultado: _argumentos.Substring(indiceTermo + termo.Length);
+            if (_argumentos.Substring(indiceTermo + termo.Length).IndexOf("&") == -1) return _argumentos.Substring(indiceTermo + termo.Length);
+            return _argumentos.Substring(indiceTermo + termo.Length).Remove(_argumentos.Substring(indiceTermo + termo.Length).IndexOf("&"));
+        }
+
+
     }
 }
--- ByteModelos/ByteBank.SistemaAgencia/Program.cs
+++ ByteModelos/ByteBank.SistemaAgencia/Program.cs
@@ -13,7 +13,11 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            
+            string urlDeTeste = "http://aoa.com/nomedamusica=likeacat&integrantes=jimin";
+            ExtratorValorDeArgumentosURL url = new ExtratorValorDeArgumentosURL(urlDeTeste);
+            string valor = "IntegRantes";
+            Console.WriteLine(url.GetValor(valor) + valor);
+            Console.ReadLine();
         }
     }
 }
